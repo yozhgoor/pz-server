9da4fae56b5e72f7bfbc625a17b24a2602601679
Yozhgoor <yozhgoor@outlook.com>
2023-11-19 21:23:36 +0100

    impl `Default` for `ServerConfig`

--- src/server_config.rs
+++ src/server_config.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 struct ServerConfig {
     /// Players can hurt and kill other players.
     ///
@@ -286,6 +288,8 @@ struct ServerConfig {
     /// Attempt to configure UPnP-enabled internet gateway to automatically setup port forwarding
     /// rules.
     /// The server will fall back to default ports if this fail.
+    ///
+    /// Default to `true`.
     upnp: bool,
     /// Enables VOIP.
     ///
@@ -293,7 +297,7 @@ struct ServerConfig {
     voice_enable: bool,
     /// The minimum tile distance over which VOIP sounds can be heard.
     ///
-    /// Minimum: `0.0`, Maximum: `100000.0`, Default: `100.0`.
+    /// Minimum: `0.0`, Maximum: `100000.0`, Default: `10.0`.
     voice_min_distance: f32,
     /// The maximum tile distance over which VOIP sounds can be heard.
     ///
@@ -306,7 +310,7 @@ struct ServerConfig {
     /// Speed limit for cars.
     ///
     /// Minimum: `10.0`, Maximum: `150.0`, Default: `70.0`.
-    speed_limit: bool,
+    speed_limit: f32,
     /// Enable a queue when too many players try to connect.
     ///
     /// Default to `false`.
@@ -333,6 +337,8 @@ struct ServerConfig {
     fast_forward_multiplier: f32,
     /// Safehouse acts like a normal house if a member of the safehouse is connected (so secure
     /// when players are offline).
+    ///
+    /// Default to `false`.
     disable_safehouse_when_player_connect: bool,
     /// Allows players to create factions.
     ///
@@ -461,7 +467,7 @@ struct ServerConfig {
     /// 3. Everyone
     ///
     /// Default to `1`.
-    map_remote_player_visibility: u8,
+    map_remote_player_visibility: Visibility,
     /// Numbers of backups.
     ///
     /// Minimum: `1`, Maximum: `300`, Default: `5`.
@@ -602,8 +608,180 @@ struct ServerConfig {
     /// Threshold value multiplier for anti-cheat protection: type 22.
     ///
     /// Minimum: `1.0`, Maximum: `10.0`, Default: `1.0`.
+    anti_cheat_protection_type_22_threshold_multiplier: f32,
     /// Threshold value multiplier for anti-cheat protection: type 24.
     ///
     /// Minimum: `1.0`, Maximum: `10.0`, Default: `6.0`.
-    anti_cheat_protection_type_24_threshold: f32,
+    anti_cheat_protection_type_24_threshold_multiplier: f32,
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            pvp: true,
+            pause_empty: true,
+            global_chat: true,
+            chat_streams: vec![
+                "s".to_owned(),
+                "r".to_owned(),
+                "a".to_owned(),
+                "w".to_owned(),
+                "y".to_owned(),
+                "sh".to_owned(),
+                "f".to_owned(),
+                "all".to_owned(),
+            ],
+            open: true,
+            server_welcome_message: "Welcome to Project Zomboid Multiplayer! <LINE> <LINE> To interact with the Chat panel: press Tab, T, or Enter. <LINE> <LINE> The Tab key will change the target stream of the message. <LINE> <LINE> Global Streams: /all <LINE> Local Streams: /say, /yell <LINE> Special Steams: /whisper, /safehouse, /faction. <LINE> <LINE> Press the Up arrow to cycle through your message history. Click the Gear icon to customize chat. <LINE> <LINE> Happy surviving!".to_owned(),
+            auto_create_user_in_white_list: false,
+            display_user_name: true,
+            show_first_and_last_name: false,
+            spawn_point: (0,0,0),
+            safety_system: true,
+            show_safety: true,
+            safety_toggle_timer: 2,
+            safety_cooldown_timer: 3,
+            spawn_items: Vec::new(),
+            default_port: 16261,
+            udp_port: 16262,
+            reset_id: 980752595,
+            mods: Vec::new(),
+            map: "Muldraugh, KY".to_owned(),
+            do_lua_checksum: true,
+            deny_login_on_overloaded_server: true,
+            public: false,
+            public_name: "My PZ Server".to_owned(),
+            public_description: String::new(),
+            max_players: 32,
+            ping_limit: 400,
+            hours_for_loot_respawn: 0,
+            max_items_for_loot_respawn: 4,
+            construction_prevents_loot_respawn: true,
+            drop_off_white_list_after_death: false,
+            no_fire: false,
+            announce_death: false,
+            minutes_per_page: 1.0,
+            save_world_every_minutes: 0,
+            player_safehouse: false,
+            admin_safehouse: false,
+            safehouse_allow_trepass: true,
+            safehouse_allow_fire: true,
+            safehouse_allow_loot: true,
+            safehouse_allow_respawn: false,
+            safehouse_day_survived_to_claim: 144,
+            safehouse_removal_time: 144,
+            safehouse_allow_non_residential: false,
+            allow_destruction_by_sledgehammer: true,
+            sledgehammer_only_in_safehouse: false,
+            kick_fast_players: false,
+            server_player_id: 0,
+            rcon_port: 27015,
+            rcon_password: String::new(),
+            discord_enable: false,
+            discord_token: String::new(),
+            discord_channel: String::new(),
+            discord_channel_id: String::new(),
+            password: String::new(),
+            max_accounts_per_user: 0,
+            allow_coop: true,
+            sleep_allowed: false,
+            sleep_needed: false,
+            knocked_down_allowed: true,
+            sneak_mode_hide_from_others_players: true,
+            workshop_items: Vec::new(),
+            steam_scoreboard: true,
+            steam_vac: true,
+            upnp: true,
+            voice_enable: true,
+            voice_min_distance: 10.0,
+            voice_max_distance: 100.0,
+            voice_3d: true,
+            speed_limit: 70.0,
+            login_queue_enable: false,
+            login_queue_connect_timeout: 60,
+            server_browser_announced_ip: String::new(),
+            player_respawn_with_self: false,
+            player_respawn_with_other: false,
+            fast_forward_multiplier: 40.0,
+            disable_safehouse_when_player_connect: false,
+            faction: true,
+            faction_day_survived_to_create: 1,
+            faction_player_required_for_tag: 1,
+            disable_radio_staff: false,
+            disable_radio_admin: true,
+            disable_radio_gm: true,
+            disable_radio_overseer: false,
+            disable_radio_moderator: false,
+            disable_radio_invisible: true,
+            client_command_filter: vec![
+                "-vehicle.*".to_owned(),
+                "+vehicle.damageWindow".to_owned(),
+                "+vehicle.fixPart".to_owned(),
+                "+vehicle.installPart".to_owned(),
+                "+vehicle.uninstallPart".to_owned(),
+            ],
+            client_action_log: vec![
+                "ISEnterVehicle".to_owned(),
+                "ISExitVehicle".to_owned(),
+                "ISTakeEngineParts".to_owned(),
+            ],
+            perk_logs: true,
+            item_numbers_limit_per_container: 0,
+            blood_splat_life_span_days: 0,
+            allow_non_ascii_username: false,
+            ban_kick_global_sound: true,
+            remove_player_corpses_on_corpse_removal: false,
+            trash_delete_all: false,
+            pvp_melee_while_hit_reaction: false,
+            mouse_over_to_see_display_name: true,
+            hide_player_behind_you: true,
+            pvp_melee_damage_modifier: 30.0,
+            pvp_firearm_damage_modifier: 50.0,
+            car_engine_attraction_modifier: 0.5,
+            player_bump_player: false,
+            map_remote_player_visibility: Visibility::Hidden,
+            backups_count: 5,
+            backups_on_start: true,
+            backups_on_version_change: true,
+            backups_period: 0,
+            anti_cheat_protection_type_1: true,
+            anti_cheat_protection_type_2: true,
+            anti_cheat_protection_type_3: true,
+            anti_cheat_protection_type_4: true,
+            anti_cheat_protection_type_5: true,
+            anti_cheat_protection_type_6: true,
+            anti_cheat_protection_type_7: true,
+            anti_cheat_protection_type_8: true,
+            anti_cheat_protection_type_9: true,
+            anti_cheat_protection_type_10: true,
+            anti_cheat_protection_type_11: true,
+            anti_cheat_protection_type_12: true,
+            anti_cheat_protection_type_13: true,
+            anti_cheat_protection_type_14: true,
+            anti_cheat_protection_type_15: true,
+            anti_cheat_protection_type_16: true,
+            anti_cheat_protection_type_17: true,
+            anti_cheat_protection_type_18: true,
+            anti_cheat_protection_type_19: true,
+            anti_cheat_protection_type_20: true,
+            anti_cheat_protection_type_21: true,
+            anti_cheat_protection_type_22: true,
+            anti_cheat_protection_type_23: true,
+            anti_cheat_protection_type_24: true,
+            anti_cheat_protection_type_2_threshold_multiplier: 3.0,
+            anti_cheat_protection_type_3_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_4_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_9_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_15_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_20_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_22_threshold_multiplier: 1.0,
+            anti_cheat_protection_type_24_threshold_multiplier: 6.0,
+        }
+    }
+}
+
+enum Visibility {
+    Hidden,
+    Friends,
+    Everyone,
 }
